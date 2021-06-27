198e761f99314253a291d1f619853ed088d4ddac
madfedeas <78121411+madfedeas@users.noreply.github.com>
2021-06-27 15:29:29 -0700

    Add files via upload

--- jazzpiano/piano_js.js
+++ jazzpiano/piano_js.js
@@ -1,32 +1,76 @@
 /*----------------------------------------------------------------------------------------------
 Author: Madison Federin-Easley
-Date: 1/19/2021
-Revision no. 5.0
+Date: 6/25/2021
+Revision no. 6.0
 ------------------------------------------------------------------------------------------------*/
 
 // Declare global variables
-let globalRoot;
-let displaykeyboard = document.forms[3];
+let globalRoot = "";
+let oldRootBtn = "";
+let oldExtBtn = "";
+let oldSclBtn = "";
+// displayKeys is the second piano on the page, where the buttons change value/color
+const displayKeys = document.forms[3];
+// Empty array to store note values to make a scale
 let oldScale = [];
+// Empty object to store note values and piano keys names that user selects
+let oldRootExts = {};
 
 /*************************************************************************************************
 chooseRoot - user clicks root key on top keyboard, which is saved and displayed in bottom keyboard
-** Inputs: root (this.value of button)
+** Inputs: root (this.value of button), note (this.id of button)
 ** Outputs: None
 **************************************************************************************************/
-function chooseRoot(root){
-    // Sets global variable
+const chooseRoot = (root, note) => {
+    // Removes class (background color) from button of previously selected root
+    if (oldRootBtn !== ""){
+        document.getElementById(oldRootBtn).classList.remove("rootbtn");
+    }
+    // Adds the class/background to the newly selected button
+    document.getElementById(note).classList.add("rootbtn");
+    // Sets the global variable
     globalRoot = root;
-    // Looping through lower keyboard/third form (which would be index 2 in the webpage)
-    for (let i = 0; i < displaykeyboard.length; i++){
-        // Values 1-12 match those of the upper keyboard
-        if (displaykeyboard.elements[i].value == globalRoot){
-            // Adds word "root" to button on new line
-            displaykeyboard.elements[i].innerText += "\nRoot";
-            // Adds class that turns text (brighter) green
-            displaykeyboard.elements[i].classList.add("root");
+    // Loop through the array of the old root and its extensions
+    for (let val in oldRootExts){
+        // And loops through the lower keyboard
+        for (let key = 0; key < displayKeys.length; key++){
+            // First removes all instances of class scl from the lower keyboard
+            // A new root will have different notes in its scales
+            displayKeys.elements[key].classList.remove("scl");
+            // When the value in the array matches a value on the lower keyboard
+            if (displayKeys.elements[key].value == val){
+                // Reset to the original button text and removes any root or extension classes (colors)
+                displayKeys.elements[key].innerText = oldRootExts[val];
+                displayKeys.elements[key].classList.remove("root");
+                displayKeys.elements[key].classList.remove("ext");
+            }
         }
     }
+    // Removes class (background color) from previously selected extension and scale buttons
+    if (oldExtBtn !== ""){
+        document.getElementById(oldExtBtn).classList.remove("extbtn");
+    }
+    if (oldSclBtn !== ""){
+        document.getElementById(oldSclBtn).classList.remove("sclbtn");
+    }
+    // Resets the object and array once all the keys have been reverted to normal
+    oldRootsExts = {};
+    oldScale = [];
+    // Looping through lower keyboard/third form (which would be index 2 in the webpage)
+    for (let i = 0; i < displayKeys.length; i++){
+        // Values 1-12 match those of the upper keyboard
+        if (displayKeys.elements[i].value == globalRoot && !displayKeys.elements[i].innerText.includes("Root")){
+            // Saves ext value and original text to object so buttons can be reset (as seen above)
+            oldRootExts[root] = displayKeys.elements[i].innerText;
+            // Adds word "Root" to button on new line
+            // Won't print "Root" if it's already present (second condition in if statement)
+            displayKeys.elements[i].innerText += "\nRoot";
+            // Adds class that turns text (brighter) green and bolds
+            displayKeys.elements[i].classList.add("root");
+        }
+    }
+    // Saves id of selected button to global variable
+    oldRootBtn = note;
 }
 
 /*************************************************************************************************
@@ -34,20 +78,33 @@ chooseExtension - user chooses extension, which is printed and displayed in bott
 ** Inputs: note (this.value of button), and ext (this.id) of button
 ** Outputs: None
 **************************************************************************************************/
-function chooseExtension(note,ext){
+const chooseExtension = (note, ext) => {
+    // Removes class (background color) from button of previously selected extension
+    if (oldExtBtn !== ""){
+        document.getElementById(oldExtBtn).classList.remove("extbtn");
+    }
+    // Adds the class/background to the newly selected button
+    document.getElementById(ext).classList.add("extbtn");
+
     // window makes dynamic variables, does the math adding number of half steps from root
     // parseFloat converting the values (strings) into numbers
     // Variable name has prefix new, then the name of the extension (ex. newmaj3)
     window['new'+ext] = parseFloat(globalRoot) + parseFloat(note);
+
     // Looping through lower keyboard/third form (which would be forms index 2 in the webpage)
-    for (let j = 0; j < displaykeyboard.length; j++){
+    for (let j = 0; j < displayKeys.length; j++){
         // Whatever value was calculated, find the matching one in the lower keyboard buttons
-        if (displaykeyboard.elements[j].value == window['new'+ext]){
-            // Adds the extension name to button on new line, class turns text red
-            displaykeyboard.elements[j].innerText += "\n" + ext;
-            displaykeyboard.elements[j].classList.add("ext");
+        if (displayKeys.elements[j].value == window['new'+ext] && !displayKeys.elements[j].innerText.includes(ext)){
+            // Adds ext value and original text to array
+            oldRootExts[window['new'+ext]] = displayKeys.elements[j].innerText;
+            // Adds the extension name to button on new line, class turns text red and bolds
+            // Won't print name if it's already present (second condition in if statement)
+            displayKeys.elements[j].innerText += "\n" + ext;
+            displayKeys.elements[j].classList.add("ext");
         }
     }
+    // Saves id of selected button to global variable
+    oldExtBtn = ext;
 }
 
 /*************************************************************************************************
@@ -55,15 +112,18 @@ chooseScale - user chooses scale based on root key, erases previously chosen sca
 ** Inputs: choice (this.value of button)
 ** Outputs: None
 **************************************************************************************************/
-function chooseScale(choice){
-    // Start by going back to previously picked scale
-    for (m in oldScale){
-        for (let n = 0; n < displaykeyboard.length; n++){
-            // Loops through bottom keyboard and removes all class instances of scl
-            // See below for when/why class is added to buttons
-            displaykeyboard.elements[n].classList.remove("scl");
-        }
+const chooseScale = (choice) => {
+    // Start by removing class (background color) from button of previously selected scale
+    if (oldSclBtn !== ""){
+        document.getElementById(oldSclBtn).classList.remove("sclbtn");
     }
+    // Adds the class/background color to the newly selected scale button
+    document.getElementById(choice).classList.add("sclbtn");
+    // Removes all previous class instances of scl so new scale can be displayed
+    for (let m = 0; m < displayKeys.length; m++){
+        displayKeys.elements[m].classList.remove("scl");
+    }
+
     let newScale = [];
     numRoot = parseFloat(globalRoot);
     // Adds root (type = number) to first element in array
@@ -115,19 +175,38 @@ function chooseScale(choice){
         default:
             break;
     }
-    // Double for loop first loops through newScale array
+    // Double for-loop first loops through newScale array
     for (k in newScale){
         // Then loops through lower keyboard
-        for (let l = 0; l < displaykeyboard.length; l++){
+        for (let l = 0; l < displayKeys.length; l++){
             // If a value in the array matches a value on the keyboard (one of the notes in the scale)
             if (document.forms[3].elements[l].value == newScale[k]){
-                // Adds class that turns that note name a different color (magenta-ish) and bolds the text
-                displaykeyboard.elements[l].classList.add("scl");
+                // Adds class that turns that note name a different color and bolds the text
+                displayKeys.elements[l].classList.add("scl");
             }
         }
     }
     // Sets current scale (newScale) to variable oldScale (see beginning of function)
     oldScale = newScale;
+    // Saves id of selected button to global variable
+    oldSclBtn = choice;
+}
+
+/*************************************************************************************************
+playAudio - plays an audio file when button is pressed
+** Inputs: id (this.id of button)
+** Outputs: None
+**************************************************************************************************/
+const playAudio = (id) => {
+    const audioDir = ".\\PianoAudio\\";
+    let audioFile = audioDir.concat(id, ".mp3");
+    let e = document.createElement("audio");
+
+    if (e.canPlayType("audio/mp3")){
+        e.setAttribute("src", audioFile);
+        e.setAttribute("autoplay", "autoplay");
+        document.body.appendChild(e);
+    }
 }
 
 /*************************************************************************************************
